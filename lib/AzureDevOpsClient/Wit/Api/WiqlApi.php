a078037560af4bedce1d95cc342074d7b4547e3c
Frank Houweling <fhouweling@senet.nl>
2019-12-16 22:40:26 +0100

    Added basic auth

--- lib/AzureDevOpsClient/Wit/Api/WiqlApi.php
+++ lib/AzureDevOpsClient/Wit/Api/WiqlApi.php
@@ -783,7 +783,11 @@ class WiqlApi
         // this endpoint requires OAuth (access token)
         if ($this->config->getAccessToken() !== null) {
             $headers['Authorization'] = 'Bearer ' . $this->config->getAccessToken();
-        }
+	}
+
+	if ($this->config->getUsername() !== null || $this->config->getPassword() !== null) {
+		            $headers['Authorization'] = 'Basic ' . base64_encode($this->config->getUsername() . ":" . $this->config->getPassword());
+	}
 
         $defaultHeaders = [];
         if ($this->config->getUserAgent()) {
